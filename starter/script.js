69d3e3cfa47c3cdef677d56998e74937a97c02d4
jamshid0705 <jamshidshamshod0705@gmail.com>
2022-04-06 12:12:18 +0500

    scriptga o'zgardi

--- starter/script.js
+++ starter/script.js
@@ -102,7 +102,7 @@
 
 // // const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-// // // const input = document.querySelector('.input');
+// const input = document.querySelector('.input');
 //const btnSearch = document.querySelector('.btn');
 // // // const form = document.querySelector('.form');
 
